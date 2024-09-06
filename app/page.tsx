b09e2d4c06819a08b486f050f2766d7fede0465a
Th3R1c0 <ricots114@protonmail.com>
2024-09-06 18:46:00 +1200

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='flex items-center justify-center flex-col h-screen'> <div>Todo app</div> 
-    <TodoApp />
-    </div>
+      <div>test test</div>
   )
 }
